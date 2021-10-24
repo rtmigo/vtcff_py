df5289a7bfb39b77c023ab324989a0b2d4b189c0
rtmigo <art.galkin@gmail.com>
2021-10-24 04:42:56 +0300

    upd

--- vtcff/__init__.py
+++ vtcff/__init__.py
@@ -1,10 +1,11 @@
 # SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
 # SPDX-License-Identifier: MIT
 
-from vtcff._common import Scale
-from vtcff._filter_crop import Crop
-from vtcff._filter_pad import Pad
-from vtcff._filter_transpose import Transpose
+from ._common import Scale
 from ._encspeed import Speed
 from ._ffmpeg_base import FfmpegCommand
+from ._filter_crop import Crop
+from ._filter_pad import Pad
+from ._filter_transpose import Transpose
 from ._math_cropping import crop_and_scale
+from ._math_padding import _letterbox
